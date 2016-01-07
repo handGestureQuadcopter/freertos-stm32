3411673b1ac30adde7d78486317258bc3c398196
CheHsuan <exas60807@gmail.com>
2016-01-07 13:41:59 +0800

    Change pwm frequency to control ESC

--- CORTEX_M4F_STM32F4/main.c
+++ CORTEX_M4F_STM32F4/main.c
@@ -39,22 +39,14 @@
 
 #include "stm32f4xx_conf.h"
 
-/* Private typedef -----------------------------------------------------------*/
-/* Private define ------------------------------------------------------------*/
-/* Private macro -------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
+#define MaxPulse 65535
+#define MinPulse 0
 
 void prvInit()
 {
 	//LCD init
-	LCD_Init();
 	IOE_Config();
 	LTDC_Cmd( ENABLE );
-
-	LCD_LayerInit();
-	LCD_SetLayer( LCD_FOREGROUND_LAYER );
-	LCD_Clear( LCD_COLOR_BLACK );
-	LCD_SetTextColor( LCD_COLOR_WHITE );
 
 	//Button
 	STM_EVAL_PBInit( BUTTON_USER, BUTTON_MODE_GPIO );
@@ -62,6 +54,7 @@ void prvInit()
 void GPIO_Configuration(void)
 {
     GPIO_InitTypeDef GPIO_InitStructure;
+
     GPIO_StructInit(&GPIO_InitStructure); // Reset init structure
 
     GPIO_PinAFConfig(GPIOD, GPIO_PinSource12, GPIO_AF_TIM4);
@@ -71,12 +64,12 @@ void GPIO_Configuration(void)
 
     
     // Setup Blue & Green LED on STM32-Discovery Board to use PWM.
-    GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15; //PD12->LED3 PD13->LED4 PD14->LED5 PD15->LED6
+    GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_12 | GPIO_Pin_13 |GPIO_Pin_14| GPIO_Pin_15; //PD12->LED3 PD13->LED4 PD14->LED5 PD15->LED6
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;            // Alt Function - Push Pull
     GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
     GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-    GPIO_Init( GPIOD, &GPIO_InitStructure );
+    GPIO_Init(GPIOD, &GPIO_InitStructure );
 
     /* UART config */
     GPIO_StructInit(&GPIO_InitStructure);
@@ -107,13 +100,11 @@ void TIM_Configuration(void)
     TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStruct;
     TIM_OCInitTypeDef TIM_OCInitStruct;
 
-    // Let PWM frequency equal 100Hz.
-    // Let period equal 1000. Therefore, timer runs from zero to 1000. Gives 0.1Hz resolution.
-    // Solving for prescaler gives 240.
+    // Let PWM frequency equal 50Hz.
     TIM_TimeBaseStructInit( &TIM_TimeBaseInitStruct );
     TIM_TimeBaseInitStruct.TIM_ClockDivision = TIM_CKD_DIV4;
-    TIM_TimeBaseInitStruct.TIM_Period = 1680 - 1;   
-    TIM_TimeBaseInitStruct.TIM_Prescaler = 500 - 1;  
+    TIM_TimeBaseInitStruct.TIM_Period = 480 - 1;   
+    TIM_TimeBaseInitStruct.TIM_Prescaler = 1000 - 1;  
     TIM_TimeBaseInitStruct.TIM_CounterMode = TIM_CounterMode_Up;        
     TIM_TimeBaseInit( TIM4, &TIM_TimeBaseInitStruct );
         
@@ -123,13 +114,22 @@ void TIM_Configuration(void)
         
     // Initial duty cycle equals 0%. Value can range from zero to 65535.
     //TIM_Pulse = TIM4_CCR1 register (16 bits)
-    TIM_OCInitStruct.TIM_Pulse = 65535; //(0=Always Off, 65535=Always On)
- 
-    TIM_OC1Init( TIM4, &TIM_OCInitStruct ); // Channel 1  LED
-    TIM_OC2Init( TIM4, &TIM_OCInitStruct ); // Channel 2  LED
-    TIM_OC3Init( TIM4, &TIM_OCInitStruct ); // Channel 3  LED
-    TIM_OC4Init( TIM4, &TIM_OCInitStruct ); // Channel 4  LED
- 
+    TIM_OCInitStruct.TIM_Pulse = 0; 
+    TIM_OC1Init(TIM4, &TIM_OCInitStruct);
+    TIM_OC1PreloadConfig(TIM4, TIM_OCPreload_Enable);
+
+    TIM_OCInitStruct.TIM_Pulse = 0; 
+    TIM_OC2Init(TIM4, &TIM_OCInitStruct);
+    TIM_OC2PreloadConfig(TIM4, TIM_OCPreload_Enable);
+
+    TIM_OCInitStruct.TIM_Pulse = 0; 
+    TIM_OC3Init(TIM4, &TIM_OCInitStruct);
+    TIM_OC3PreloadConfig(TIM4, TIM_OCPreload_Enable);
+
+    TIM_OCInitStruct.TIM_Pulse = 0; 
+    TIM_OC4Init(TIM4, &TIM_OCInitStruct);
+    TIM_OC4PreloadConfig(TIM4, TIM_OCPreload_Enable); 
+
     TIM_Cmd( TIM4, ENABLE );
 }
 
@@ -137,15 +137,6 @@ void USART1_Configuration(void)
 {
     USART_InitTypeDef USART_InitStructure;
 
-    /* USARTx configuration ------------------------------------------------------*/
-    /* USARTx configured as follow:
-     *  - BaudRate = 9600 baud
-     *  - Word Length = 8 Bits
-     *  - One Stop Bit
-     *  - No parity
-     *  - Hardware flow control disabled (RTS and CTS signals)
-     *  - Receive and transmit enabled
-     */
     USART_InitStructure.USART_BaudRate = 115200;
     USART_InitStructure.USART_WordLength = USART_WordLength_8b;
     USART_InitStructure.USART_StopBits = USART_StopBits_1;
@@ -165,27 +156,60 @@ void USART1_puts(char* s)
     }
 }
 
-int atoi(const char *str)
+void USART1_int(uint16_t num)
 {
-	int i = 0, num = 0, sign = 0;
-	if (str[i] == '-') {
-		i++;
-		sign = 1;
+    //The range of unsigned integer is from 0 to 65535,which max length is 5 digits. 
+    char intArray[5];
+    int i;
+    for(i = 4; i >= 0 ; i--) {
+        intArray[i] = num % 10 + 48;
+        num = num / 10;
+        if(num == 0)
+            break;
+    }
+    USART1_puts(&intArray[i]);
+}
+
+uint16_t myatoi(char *str)
+{	
+	uint16_t num = 0;
+	while (*str != '\0') {
+		num *= 10; 
+		num += (*str - '0');
+		str++;
 	}
-	while (str[i] >= '0' && str[i] <= '9') {
-		num = num * 10 + str[i] - '0';
-		i++;
-	}
-	if (sign)
-		num = -num;
 	return num;
 }
 
 void command_detect(char *str)
 {
-	unsigned int number;
-	number = atoi(str);
-	TIM4->CCR1 = number;
+	uint16_t brightness;
+	uint16_t channel;
+	brightness = myatoi(str);
+	channel = brightness / 1000;
+	brightness %= 1000;
+	switch(channel){
+		case 1:
+			TIM4->CCR1 = brightness;
+			USART1_puts("channel 1 :\t\0");
+			USART1_int(brightness);	
+			break;
+		case 2:
+			TIM4->CCR2 = brightness;
+			USART1_puts("channel 2 :\t\0");
+			USART1_int(brightness);
+			break;
+		case 3:
+			TIM4->CCR3 = brightness;
+			USART1_puts("channel 3 :\t\0");
+			USART1_int(brightness);
+			break;
+		case 4:
+			TIM4->CCR4 = brightness;
+			USART1_puts("channel 4 :\t\0");
+			USART1_int(brightness);
+			break;
+	}
 }
 
 void uart1command() {
@@ -201,15 +225,7 @@ void uart1command() {
 			USART1_puts(buffer);
 			command_detect(buffer);
 			index = 0;
-		} else if (buffer[index] == 8 || buffer[index] == 127) {
-			if (index != 0) {
-				USART1_puts("\b");
-				USART1_puts(" ");
-				USART1_puts("\b");
-				index--;
-			}
-		}
-		else{
+		} else{
 			buffer[index++] = c;
 		}
 		if(index == 50)
@@ -224,59 +240,18 @@ void UART1Task(void *pvParameters)
 	uart1command();
 }
 
-void PWMTask(void *pvParameters)
-{
-	volatile int i = 0;
-	//int whichpin = *(int *)pvParameters;
-	uint16_t whichpin = 0;
-	uint16_t brightness = 3000;
-	int n = 1;
-	while(1){
-		
-		//if(brightness + n <=0)
-		//	whichpin = (whichpin + 1) % 4;
-	
-		if (((brightness + n) >= 3000) || ((brightness + n) <= 0))
-			n = -n; // if  brightness maximum/maximum change direction
-
-		//brightness += n;
-
-		//Light LEDs in turn
-		switch(whichpin){
-			case 0:
-				TIM4->CCR1 = brightness; // set brightness
-				break;
-			case 1:
-				TIM4->CCR2 = brightness; // set brightness
-				break;
-			case 2:
-				TIM4->CCR3 = brightness; // set brightness
-				break;
-			case 3:
-				TIM4->CCR4 = brightness; // set brightness
-				break;
-		}
-		for(i=0;i<10000;i++);  // delay
-	}
-}
-
 //Main Function
 int main(void)
 {
-	int whichpin[4] = {0,1,2,3};
-	
 	RCC_Configuration();
 	TIM_Configuration();
 	GPIO_Configuration();
 	USART1_Configuration();
 
 	prvInit();
-
-	xTaskCreate(PWMTask, "PWM1 Task", 256, 
-		( void * ) whichpin, tskIDLE_PRIORITY + 1,(void *)NULL);
+			
 	xTaskCreate(UART1Task, "UART1", 256,
-		(void *) whichpin, tskIDLE_PRIORITY + 2, (void *)NULL);
+		(void *)NULL, tskIDLE_PRIORITY + 1, (void *)NULL);
 	
 	vTaskStartScheduler();
 }
-
