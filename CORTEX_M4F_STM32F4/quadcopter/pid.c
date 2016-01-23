febe6fb63802a7ef384ce0d6d3d8e29e4187ee2d
CheHsuan <exas60807@gmail.com>
2016-01-23 20:35:16 +0800

    Delay PID task for waiting ESC ready

--- CORTEX_M4F_STM32F4/quadcopter/pid.c
+++ CORTEX_M4F_STM32F4/quadcopter/pid.c
@@ -1,48 +1,51 @@
 #include "pid.h"
+#include "uart.h"
 
 extern MotorSpeed_t motorspeed;
 extern Kalman_Angel_Data K_Data;
 
 float roll, pitch;
 
-uint8_t PID_Task_Creat() {
-	BaseType_t ret = xTaskCreate(PIDTask,"PID",512,(void *) NULL,tskIDLE_PRIORITY + 4,(void *) NULL);
+uint8_t PID_Task_Creat(TaskHandle_t xHandle) {
+	BaseType_t ret = xTaskCreate(PIDTask,"PID",512,(void *) NULL,tskIDLE_PRIORITY + 4, &xHandle);
 	if (ret != pdPASS)  
 		return 0;
 	return 1;   
 }
 
-
-void  PIDTask()
+void PIDTask()
 {	
 	int16_t pre_error_X, pre_error_Y;
 	int16_t integral_X, integral_Y;
 	int16_t error_X, error_Y;
 	int16_t derivative_X, derivative_Y;
 
-	//Intialize
-	
+	//Intialize	
 	pre_error_X = 0;
 	integral_X = 0;
 	
 	pre_error_Y = 0;
 	integral_Y = 0;
 
+	//wait for initializing	ESC
+	UART1_puts("WAIT FOR ESC\r\n\0");
+	vTaskDelay(5000);
+	UART1_puts("Start PID\r\n\0");
+	
 	while(1){
-
 		taskENTER_CRITICAL();
 		roll = K_Data.kalAngleX;
 		pitch = K_Data.kalAngleY;
 		taskEXIT_CRITICAL();
 
-		//x axis	
+		//x axis for roll	
 		error_X = calculateP_X(roll);
 		integral_X = calculateI(integral_X, error_X);
 		derivative_X = calculateD(error_X, pre_error_X);
 		PID_X(error_X,integral_X,derivative_X);
 		pre_error_X = error_X;
 
-		//y axis
+		//y axis for pitch
 		error_Y = calculateP_Y(pitch);
 		integral_Y = calculateI(integral_Y, error_Y);
 		derivative_Y = calculateD(error_Y, pre_error_Y);
@@ -57,21 +60,21 @@ void  PIDTask()
 float calculateP_X(float roll)
 {
 	float error;
-	error = roll - SETPOINT_X;
+	error = SETPOINT_X - roll;
 	return error;
 }
 
 float calculateP_Y(float pitch)
 {
 	float error;
-	error = pitch - SETPOINT_Y;
+	error = SETPOINT_Y - pitch;
 	return error;
 }
 
 
 float calculateI(float integral, float error)
 {		
-	return (0.66) * integral + (error * dt);
+	return (0.66) * (integral + (error * dt));
 }
 
 float calculateD(float error, float pre_error)	
@@ -83,20 +86,21 @@ void PID_X(float error, float integral, float derivative)
 {
 	float output;
 	output = (KP * error) + (KI * integral) + (KD * derivative);
-
-	motorspeed.magicNumber1 -= output;
-	motorspeed.magicNumber2 += output;
-	motorspeed.magicNumber3 += output;
-	motorspeed.magicNumber4 -= output;
+	output = PID_BOUND(output);
+	motorspeed.magicNumber1 = MAGIC_CHECK(motorspeed.magicNumber1 + output);
+	motorspeed.magicNumber2 = MAGIC_CHECK(motorspeed.magicNumber2 - output);
+	motorspeed.magicNumber3 = MAGIC_CHECK(motorspeed.magicNumber3 - output);
+	motorspeed.magicNumber4 = MAGIC_CHECK(motorspeed.magicNumber4 + output);
 }
 
 void PID_Y(float error, float integral, float derivative)
 {
 	float output;
 	output = (KP * error) + (KI * integral) + (KD * derivative);
+	output = PID_BOUND(output);
+	motorspeed.magicNumber1 = MAGIC_CHECK(motorspeed.magicNumber1 - output);
+	motorspeed.magicNumber2 = MAGIC_CHECK(motorspeed.magicNumber2 - output);
+	motorspeed.magicNumber3 = MAGIC_CHECK(motorspeed.magicNumber3 + output);
+	motorspeed.magicNumber4 = MAGIC_CHECK(motorspeed.magicNumber4 + output);
 
-	motorspeed.magicNumber1 += output;
-	motorspeed.magicNumber2 += output;
-	motorspeed.magicNumber3 -= output;
-	motorspeed.magicNumber4 -= output; 
 }
